249b3213fdee7a986e0bddc182b17c7042529daa
shubham10divakar <shubham.divakar@gmail.com>
2024-06-28 22:11:41 +0530

    Details of project added in list

---
+++ src/components/pages/ListOfProjects.js
@@ -0,0 +1,85 @@
+const projectData = [{
+    id:1,
+    title: 'Network Analysis Tool',
+    description: ' helps to analyze multiple HAR files (HTTPS Archive) which has info of the API class.',
+    owner: 'Subham Divakar(Me)',
+    startDate: '2024-01-01',
+    endDate: 'Constantly Improving',
+    skills: ['Python'],
+    links: {
+      youtube: 'https://www.youtube.com/watch?v=GkFWvz0an48', // Replace with your actual YouTube video URL
+      github: 'https://github.com/example/project', // Replace with your actual GitHub repository URL
+    },
+    installation: 'You can install this project using pip. Run the following command:\n\n```pip install your-package```',
+    usageExamples: [
+      {
+        title: 'Example 1',
+        description: 'Description of example 1 usage.',
+        code: 'Your usage code snippet for example 1',
+      },
+      {
+        title: 'Example 2',
+        description: 'Description of example 2 usage.',
+        code: 'Your usage code snippet for example 2',
+      },
+    ],
+    features: [
+      'It helps to analyze multiple HAR files (HTTPS Archive) which has info of the API class',
+      'Feature 2',
+      'Feature 3',
+    ],
+    contributing: 'Thank you for considering contributing to our project! Please read our [Contributing Guidelines](link-to-contributing-docs) for details on how to get started.',
+    communityLinks: {
+      forum: 'Link to community forum or mailing list',
+      support: 'Link to support channel or FAQ page',
+    },
+    acknowledgments: 'We acknowledge the contributions of individuals or organizations that have helped make this project possible.',
+    screenshots: [
+      'URL to screenshot or demo GIF 1',
+      'URL to screenshot or demo GIF 2',
+    ],
+  }, 
+  {
+    id : 2,
+    title: 'My Project Title 2',
+    description: 'This is a detailed description of my project. It includes the objectives, scope, and important information.',
+    owner: 'Your Name',
+    startDate: '2023-01-01',
+    endDate: '2023-12-31',
+    skills: ['React', 'JavaScript', 'CSS', 'Material-UI', 'Python'],
+    links: {
+      youtube: 'https://www.youtube.com/watch?v=GkFWvz0an48', // Replace with your actual YouTube video URL
+      github: 'https://github.com/shubham10divakar/Network-Analysis-Tool', // Replace with your actual GitHub repository URL
+    },
+    installation: 'You can install this project using pip. Run the following command:\n\n```pip install your-package```',
+    usageExamples: [
+      {
+        title: 'Example 1',
+        description: 'Description of example 1 usage.',
+        code: 'Your usage code snippet for example 1',
+      },
+      {
+        title: 'Example 2',
+        description: 'Description of example 2 usage.',
+        code: 'Your usage code snippet for example 2',
+      },
+    ],
+    features: [
+      'Feature 1',
+      'Feature 2',
+      'Feature 3',
+    ],
+    contributing: 'Thank you for considering contributing to our project! Please read our [Contributing Guidelines](link-to-contributing-docs) for details on how to get started.',
+    communityLinks: {
+      forum: 'Link to community forum or mailing list',
+      support: 'Link to support channel or FAQ page',
+    },
+    acknowledgments: 'We will acknowledge the contributions of individuals  that will help to make this project possible.',
+    screenshots: [
+      'URL to screenshot or demo GIF 1',
+      'URL to screenshot or demo GIF 2',
+    ],
+  }];
+
+
+  export default projectData;
--- src/components/pages/ProjectDetailsView.js
+++ src/components/pages/ProjectDetailsView.js
@@ -4,56 +4,86 @@ import { Container, Card, CardContent, Typography, Avatar, Button } from '@mui/m
 //import { useTheme } from './CustomThemeProvider';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Import Font Awesome
+import { faPython, faJs, faHtml5, faCss3Alt, faReact } from '@fortawesome/free-brands-svg-icons'; // Import specific icons
+// Import the Python logo image (adjust path as needed)
+import pythonLogo from '../../images/python-logo.png';
+import { useParams } from 'react-router-dom';
+import projectDataList from './ListOfProjects';
 
-const projectData = {
-    title: 'My Project Title',
-    description: 'This is a detailed description of my project. It includes the objectives, scope, and important information.',
-    owner: 'Your Name',
-    startDate: '2023-01-01',
-    endDate: '2023-12-31',
-    skills: ['React', 'JavaScript', 'CSS', 'Material-UI'],
-    links: {
-      youtube: 'https://www.youtube.com/watch?v=GkFWvz0an48', // Replace with your actual YouTube video URL
-      github: 'https://github.com/example/project', // Replace with your actual GitHub repository URL
-    },
-    installation: 'You can install this project using pip. Run the following command:\n\n```pip install your-package```',
-    usageExamples: [
-      {
-        title: 'Example 1',
-        description: 'Description of example 1 usage.',
-        code: 'Your usage code snippet for example 1',
-      },
-      {
-        title: 'Example 2',
-        description: 'Description of example 2 usage.',
-        code: 'Your usage code snippet for example 2',
-      },
-    ],
-    features: [
-      'Feature 1',
-      'Feature 2',
-      'Feature 3',
-    ],
-    contributing: 'Thank you for considering contributing to our project! Please read our [Contributing Guidelines](link-to-contributing-docs) for details on how to get started.',
-    communityLinks: {
-      forum: 'Link to community forum or mailing list',
-      support: 'Link to support channel or FAQ page',
-    },
-    acknowledgments: 'We acknowledge the contributions of individuals or organizations that have helped make this project possible.',
-    screenshots: [
-      'URL to screenshot or demo GIF 1',
-      'URL to screenshot or demo GIF 2',
-    ],
-  };
+// const projectData = {
+//     title: 'My Project Title',
+//     description: 'This is a detailed description of my project. It includes the objectives, scope, and important information.',
+//     owner: 'Your Name',
+//     startDate: '2023-01-01',
+//     endDate: '2023-12-31',
+//     skills: ['React', 'JavaScript', 'CSS', 'Material-UI', 'Python'],
+//     links: {
+//       youtube: 'https://www.youtube.com/watch?v=GkFWvz0an48', // Replace with your actual YouTube video URL
+//       github: 'https://github.com/example/project', // Replace with your actual GitHub repository URL
+//     },
+//     installation: 'You can install this project using pip. Run the following command:\n\n```pip install your-package```',
+//     usageExamples: [
+//       {
+//         title: 'Example 1',
+//         description: 'Description of example 1 usage.',
+//         code: 'Your usage code snippet for example 1',
+//       },
+//       {
+//         title: 'Example 2',
+//         description: 'Description of example 2 usage.',
+//         code: 'Your usage code snippet for example 2',
+//       },
+//     ],
+//     features: [
+//       'Feature 1',
+//       'Feature 2',
+//       'Feature 3',
+//     ],
+//     contributing: 'Thank you for considering contributing to our project! Please read our [Contributing Guidelines](link-to-contributing-docs) for details on how to get started.',
+//     communityLinks: {
+//       forum: 'Link to community forum or mailing list',
+//       support: 'Link to support channel or FAQ page',
+//     },
+//     acknowledgments: 'We acknowledge the contributions of individuals or organizations that have helped make this project possible.',
+//     screenshots: [
+//       'URL to screenshot or demo GIF 1',
+//       'URL to screenshot or demo GIF 2',
+//     ],
+//   };
   
   const ProjectDetailsView = () => {
     //const { isDark, toggleTheme } = useTheme();
-  
+
+    const { id } = useParams();
+    console.log(id);
+    //console.log(projectDataList[1])
+    console.log(projectDataList[id-1])
+    const projectData = projectDataList[id-1]
     // Function to extract YouTube video ID from URL
     const getYouTubeVideoId = (url) => {
       const match = url.match(/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/);
       return match && match[1];
     };
+
+    // Function to map programming languages to their respective icons
+  const getLanguageIcon = (language) => {
+    switch (language.toLowerCase()) {
+      case 'python':
+        return <FontAwesomeIcon icon={faPython} />;
+      case 'javascript':
+        return <FontAwesomeIcon icon={faJs} />;
+      case 'html':
+        return <FontAwesomeIcon icon={faHtml5} />;
+      case 'css':
+        return <FontAwesomeIcon icon={faCss3Alt} />;
+      case 'react':
+        return <FontAwesomeIcon icon={faReact} />;
+      // Add more cases for other languages as needed
+      default:
+        return null;
+    }
+  };
   
     const youtubeId = getYouTubeVideoId(projectData.links.youtube);
   
@@ -89,6 +119,23 @@ const projectData = {
                 </li>
               ))}
             </ul>
+            <div className="section">
+              <Typography variant="h6">
+                <strong>Check below video for what? why? and How? to use the tool:</strong>
+              </Typography>
+              <div className="video-container">
+                <iframe
+                  width="560"
+                  height="315"
+                  src={`https://www.youtube.com/embed/${youtubeId}`}
+                  title="YouTube video player"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  className="embedded-video"
+                ></iframe>
+              </div>
+            </div>
             <div className="links-section">
               <Typography variant="h6">
                 <strong>Links:</strong>
@@ -101,6 +148,19 @@ const projectData = {
                 <GitHubIcon className="link-icon" />
                 GitHub Repository
               </a>
+            </div>
+            <div className="section">
+            <Typography variant="h6">
+              <strong>Languages:</strong>
+            </Typography>
+            <div className="language-icons">
+              {projectData.skills.map((language, index) => (
+                <div key={index} className="language-icon">
+                  {getLanguageIcon(language)}
+                  <Typography variant="caption">{language}</Typography>
+                </div>
+                ))}
+                </div>
             </div>
             <div className="section">
               <Typography variant="h6">
@@ -164,23 +224,6 @@ const projectData = {
               <Typography>
                 {projectData.acknowledgments}
               </Typography>
-            </div>
-            <div className="section">
-              <Typography variant="h6">
-                <strong>Embedded YouTube Video:</strong>
-              </Typography>
-              <div className="video-container">
-                <iframe
-                  width="560"
-                  height="315"
-                  src={`https://www.youtube.com/embed/${youtubeId}`}
-                  title="YouTube video player"
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                  className="embedded-video"
-                ></iframe>
-              </div>
             </div>
             <Card variant="outlined" className="avatar-container">
               <CardContent>
