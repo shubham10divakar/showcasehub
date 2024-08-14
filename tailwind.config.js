74ab9c291add3c6fbca9e2e228fcc467ea0bc2ef
shubham10divakar <shubham.divakar@gmail.com>
2024-08-14 14:26:06 +0530

    Added tailwind but not using it, will use in later modules

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust this path to your project's structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
