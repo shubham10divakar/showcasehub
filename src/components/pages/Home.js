448356b9460ec2fa41def63f45a7125e586d02e4
shubham10divakar <shubham.divakar@gmail.com>
2024-09-14 10:16:07 +0530

    Minor fixes

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import '../../App.css';
+//import '../../App.css';
 import Cards from '../Cards';
 import HeroSection from '../HeroSection';
 import Footer from '../Footer';
